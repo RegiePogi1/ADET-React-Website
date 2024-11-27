0bb9c9b770ee7776b6031260da901c097062030d
RegieN <146059704+RegiePogi1@users.noreply.github.com>
2024-11-27 12:09:47 +0800

    Update contact.jsx

--- src/contact.jsx
+++ src/contact.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Catto from './assets/catto.png';
 
 function Contacts() {
   return (
     <div class="form">
       <h1>Contact Me</h1>
       <div class="container1">
-      <a href="https://www.facebook.com/namora.regie1">Facebook - Regie Namora</a> <img src='assets/catto.png' class="logo"></img><br></br>
+      <a href="https://www.facebook.com/namora.regie1">Facebook - Regie Namora</a> <img src={Catto} class="logo"></img><br></br>
       <a href="https://github.com/RegiePogi1">Github - RegiePogi1 <img src='assets/catto.png' class="logo"></img> </a>
       </div>
     </div>
