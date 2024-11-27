b524eec80e49ba9dc2d7007fb224887dd7f407dd
RegieN <146059704+RegiePogi1@users.noreply.github.com>
2024-11-27 12:33:48 +0800

    Update about.jsx

--- src/about.jsx
+++ src/about.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import Regie from './assets/Regie.jpg';
+
 function About() {
   return (
-    <form class="form2">
-      <h1>
-        About
-      </h1>
-      <img src={Regie} class="logo1"></img>
+    <div className="form2">
+      <h1>About</h1>
+      <img src={Regie} alt="Regie" className="logo1" />
+      <p>I'm Regie Namora. I'm the creator of this Website.</p>
       <p>
-        I'm Regie Namora. I'm the creator of this Website
+        This Website is my Performance Task #9 for <strong>Application Development</strong>.
       </p>
-      <p>This Website is my Performance Task #9 for <strong> Application Development </strong></p>
-      <p>I'm currently studying at Naga College Foundation with a Course of Computer Science</p>
-      <p>My goal is to become a <strong>Programmer</strong></p>
-</form>
+      <p>
+        I'm currently studying at Naga College Foundation, pursuing a course in Computer Science.
+      </p>
+      <p>My goal is to become a <strong>Programmer</strong>.</p>
+    </div>
   );
 }
 
